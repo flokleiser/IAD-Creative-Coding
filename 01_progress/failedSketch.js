2d875405e75e5300bcffef290d300623602ea714
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-11-22 12:34:29 +0100

    some fix

--- 01_progress/failedSketch.js
+++ 01_progress/failedSketch.js
@@ -6,11 +6,10 @@ let bigWordPixels = [];
 let logoImg;
 let imgAspectRatio;
 let imgWidth, imgHeight;
-
+let gridSpacing
 
 function preload() {
   font = loadFont('/98_media/fonts/Neue-HaasGroteskDispW0496BlkIt.otf');
-  logoImg = loadImage('/98_media/logos/slim/logo_slim.png');
 }
 
 function setup() {
@@ -19,24 +18,26 @@ function setup() {
   textAlign(CENTER, CENTER);
   textFont(font);
 
-  imgAspectRatio = logoImg.height / logoImg.width;
-
-  if (windowWidth / windowHeight > logoImg.width / logoImg.height) {
-      imgHeight = windowHeight;
-      imgWidth = windowHeight / imgAspectRatio;
-    } else {
-      imgWidth = windowWidth;
-      imgHeight = windowWidth * imgAspectRatio;
-    }
-
-    logoImg.resize(windowWidth,windowHeight)
-    logoImg.loadPixels();
+  bigWordImg = createGraphics(windowWidth, windowHeight);
+  bigWordImg.textFont(font);
+//   bigWordImg.textSize(windowWidth/3);
+  bigWordImg.textSize(500);
+  bigWordImg.textAlign(CENTER, CENTER);
+  bigWordImg.background(0);
+  bigWordImg.fill(255);
+  bigWordImg.text(bigWord, bigWordImg.width / 2, bigWordImg.height / 2);
+  bigWordImg.loadPixels();
 }
 
 function draw() {
   background(0);
   let index = 0;
-  let gridSpacing = 18;
+  // if (windowHeight < windowWidth) {
+  //   gridSpacing = windowHeight/40;
+  // } else {
+  //   gridSpacing = windowWidth/40;
+  // }
+  gridSpacing = 50
 
   mouseRadius = lerp(mouseRadius, targetRadius, 0.1);
 
@@ -49,38 +50,35 @@ function draw() {
         let distance = dist(mouseX, mouseY, x, y);
         let size = gridSpacing/width;
 
-        let imgX = floor(map(x, (width - imgWidth) / 2, (width + imgWidth) / 2, 0, logoImg.width));
-        let imgY = floor(map(y, (height - imgHeight) / 2, (height + imgHeight) / 2, 0, logoImg.height));
-
-        let pixelIndex = (imgY * logoImg.width + imgX) * 4;
-        let r = logoImg.pixels[pixelIndex];
-        let g = logoImg.pixels[pixelIndex + 1];
-        let b = logoImg.pixels[pixelIndex + 2];
-
-
-
-          if (distance < mouseRadius) {
+        let imgX = floor(map(x, 0, width, 0, bigWordImg.width));
+        let imgY = floor(map(y, 0, height, 0, bigWordImg.height));
+        let pixelIndex = (imgY * bigWordImg.width + imgX) * 4;
+        let r = bigWordImg.pixels[pixelIndex];
+        let g = bigWordImg.pixels[pixelIndex + 1];
+        let b = bigWordImg.pixels[pixelIndex + 2];
+  
+        if (distance < mouseRadius) {
             if (r < 128 && g < 128 && b < 128) {
               size += map(distance, 0, mouseRadius, 10, 2);
             } else {
-              size = 1 
+              size -= 1
             }
           } else {
-            size += 3;
-            // size += 2 
+            size += 4
           }
+      
 
         textSize(size * 4);
         text(letter, x, y - (textAscent() + textDescent()) / 4);
         index++;
     }
-  }
+    }
 }
 
 function mousePressed() { 
-  targetRadius = 500;
+  targetRadius = 700;
 }
 
 function mouseReleased() { 
-  targetRadius = 100;
+  targetRadius = 200;
 }
